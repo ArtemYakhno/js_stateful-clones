328180f5d01a69b0ee1a346d85c976927e210627
Artem Yakhno <artem.yakhno.ua@gmail.com>
2025-09-17 23:36:56 +0300

    fixed isuess

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,70 +8,47 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const stateCopy = { ...state };
+  let stateCopy = { ...state };
   const result = [];
 
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
         addProperties(stateCopy, action.extraData);
-        result.push({ ...stateCopy }); // робимо копію
         break;
 
       case 'removeProperties':
         removeProperties(stateCopy, action.keysToRemove);
-        result.push({ ...stateCopy }); // робимо копію
         break;
 
       case 'clear':
-        clearProperties(stateCopy);
-        result.push({ ...stateCopy }); // робимо копію
+        stateCopy = {};
         break;
 
       default:
-        return;
+        throw new Error('Unsupported action type: ' + action.type);
     }
+
+    result.push({ ...stateCopy });
   }
 
   return result;
 }
 
 function addProperties(state, extraData) {
-  Object.assign(state, extraData);
+  if (extraData && typeof extraData === 'object') {
+    Object.assign(state, extraData);
+  }
 }
 
 function removeProperties(state, keysToRemove) {
+  if (!Array.isArray(keysToRemove) || keysToRemove.length === 0) {
+    return;
+  }
+
   for (const key of keysToRemove) {
     delete state[key];
   }
 }
-
-function clearProperties(state) {
-  for (const key in state) {
-    delete state[key];
-  }
-}
-
-const currentState = {
-  foo: 'bar',
-  bar: 'foo',
-};
-
-const stateHistory = transformStateWithClones(currentState, [
-  {
-    type: 'addProperties',
-    extraData: { name: 'Jim', hello: 'world' },
-  },
-  {
-    type: 'removeProperties',
-    keysToRemove: ['bar', 'hello'],
-  },
-  {
-    type: 'addProperties',
-    extraData: { another: 'one' },
-  },
-]);
-
-console.log(stateHistory);
 
 module.exports = transformStateWithClones;
