31a230d267ab82a01a636194ae7b4f3d1f61ed93
Artem Yakhno <artem.yakhno.ua@gmail.com>
2025-09-17 23:07:48 +0300

    added solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 'use strict';
 
 /**
@@ -7,7 +8,70 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        addProperties(stateCopy, action.extraData);
+        result.push({ ...stateCopy }); // робимо копію
+        break;
+
+      case 'removeProperties':
+        removeProperties(stateCopy, action.keysToRemove);
+        result.push({ ...stateCopy }); // робимо копію
+        break;
+
+      case 'clear':
+        clearProperties(stateCopy);
+        result.push({ ...stateCopy }); // робимо копію
+        break;
+
+      default:
+        return;
+    }
+  }
+
+  return result;
 }
+
+function addProperties(state, extraData) {
+  Object.assign(state, extraData);
+}
+
+function removeProperties(state, keysToRemove) {
+  for (const key of keysToRemove) {
+    delete state[key];
+  }
+}
+
+function clearProperties(state) {
+  for (const key in state) {
+    delete state[key];
+  }
+}
+
+const currentState = {
+  foo: 'bar',
+  bar: 'foo',
+};
+
+const stateHistory = transformStateWithClones(currentState, [
+  {
+    type: 'addProperties',
+    extraData: { name: 'Jim', hello: 'world' },
+  },
+  {
+    type: 'removeProperties',
+    keysToRemove: ['bar', 'hello'],
+  },
+  {
+    type: 'addProperties',
+    extraData: { another: 'one' },
+  },
+]);
+
+console.log(stateHistory);
 
 module.exports = transformStateWithClones;
